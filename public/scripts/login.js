153005ea03b89b4a80ff9759b177b78ec19ce577
Juan Escalada <juanescalada175@gmail.com>
2022-10-01 10:55:34 -0700

    Add login page script and profile page template

---
+++ public/scripts/login.js
@@ -0,0 +1,47 @@
+function login() {
+    let data = {
+        username: $("#username").val(),
+        password: $("#password").val()
+    }
+
+    fetch('/login', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }).then(response => response.json()).then((data) => {
+        // If authentication was successful, redirect to user profile, else display an error message
+        if (data.success) {
+            sessionStorage.setItem("userId", data.user.user_id);
+            sessionStorage.setItem("isAdmin", data.user.isAdmin);
+            sessionStorage.setItem("authenticated", data.success);
+            window.location.href = '/profile'
+        } else {
+            $("#error-text").text("Invalid credentials. Please try again.")
+        }
+    })
+}
+
+function register() {
+    let data = {
+        username: $("#username").val(),
+        password: $("#password").val(),
+        isAdmin: document.getElementById("isAdmin").checked
+    }
+
+    fetch('/register', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }).then(response => response.json()).then((data) => {
+        // On success, redirect (refresh) to login
+        if (data.success) {
+            $("#success-text").text("Account created. You can log in now.")
+        } else {
+            $("#error-text").text("Account creation failed.")
+        }
+    })
+}
