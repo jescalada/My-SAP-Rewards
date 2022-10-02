68a10e2e32d1156e8183da05944d92c364a07a3d
Juan Escalada <juanescalada175@gmail.com>
2022-10-02 09:17:12 -0700

    Fix bar issue

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -71,7 +71,7 @@ async function loadRewards() {
                     </button>
                     <button type="button"
                         class="py-2 px-4 text-sm font-medium text-gray-900 bg-green-400 rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-                        Redeem
+                        ${reward.reward_cost > "Redeem"}
                     </button>
                 </div>`;
 
