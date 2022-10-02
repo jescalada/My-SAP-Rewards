df3a9d4fb59176186c08e4762ce111691c8ceb9b
Juan Escalada <juanescalada175@gmail.com>
2022-10-02 14:41:27 -0700

    Bugfix schedule backend

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -1,6 +1,10 @@
 let userId = getUserId();
+let currentDay = 1;
+let fullName = getFullName();
 
 async function loadDate(dateNumber) {
+    currentDay = dateNumber;
+
     let data = {
         dateNumber: dateNumber,
         location: "SAP Canada Inc.",
@@ -24,7 +28,7 @@ async function loadDate(dateNumber) {
 
         $("#attendees-box").empty();
         $("#day-status").text("Remote");
-        
+
         data.users.forEach(user => {
             let element = `
                 <div class="border-b pb-4 border-gray-400 border-dashed">
@@ -38,6 +42,30 @@ async function loadDate(dateNumber) {
             $("#attendees-box").append(element);
         });
     })
+}
+
+async function setInPerson() {
+    $(`#day-${currentDay}`).addClass("bg-green-700");
+    $(`#day-${currentDay}`).removeClass("bg-red-700");
+    let element = `
+                <div class="border-b pb-4 border-gray-400 border-dashed">
+                    <p class="text-xs font-light leading-3 text-gray-500 dark:text-gray-300">9:00AM - 5:30PM</p>
+                    <a tabindex="0" class="focus:outline-none text-lg font-medium leading-5 text-gray-800 dark:text-gray-100 mt-2">${fullName}</a>
+                </div>
+            `;
+    $("#attendees-box").append(element);
+    $("#day-status").text("In-Person");
+}
+
+async function setRemote() {
+    $(`#day-${currentDay}`).addClass("bg-red-700");
+    $(`#day-${currentDay}`).removeClass("bg-green-700");
+    $("#day-status").text("Remote");
+    // todo update list
+}
+
+async function setUndecided() {
+    console.log("Undecided triggered")
 }
 
 $(".date").on( "click", function() {
