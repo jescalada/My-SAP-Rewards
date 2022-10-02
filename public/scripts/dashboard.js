cbadbbbc70a192f0017aa2e65472d60fd9cbcb4f
Juan Escalada <juanescalada175@gmail.com>
2022-10-02 09:50:20 -0700

    Add dynamic dashboard points

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,6 +1,4 @@
-async function loadRewards() {
-
-}
+var userId = getUserId();
 
 async function loadPoints() {
     let data = {
@@ -14,41 +12,21 @@ async function loadPoints() {
             'Content-type': 'application/json'
         }
     }).then(response => response.json()).then(async (data) => {
-                data.points;
-                data.desired_reward_cost;
-                $("#username3").text(data.username);
-                $("#username4").text(data.username);
-            data.rewards_pending.forEach(async (reward, index) => {
-                console.log(reward);
-                
-                let date = new Date(reward.redeem_date)
-                let dateTime = date.toString().split(" ")
-                let element = `
-                    <div class="order" id="order-${index + 1}" style="text-align: center">
-                        <h3>${reward.reward_name}</h3>
-                        <h2>${dateTime[1] + " " + dateTime[2] + ", " + dateTime[3]}</h2>
-                        <p class="details">${reward.reward_desc}</p>
-                    </div>`;
-                $("#pending-rewards").append(element);
-
-                /*order[0].cart.forEach(async (pokemon) => {
-                    let pokemonData = await getPokemonBasicDataById(pokemon.pokemonId)
-                    let entry = `
-                    <div class="thumbnail-container" style="text-align: center; display: inline-block">
-                        <img src="${pokemonData.sprite}" alt="${pokemonData.name}" style="width:100%"
-                            onclick="location.href='pokemon.html?id=${pokemonData.id}'" class="pokemon-image-thumb">
-                            <div class="row pokemon-buy-details">
-                            <h3 class="col card-price">${pokemonData.name}</h3>
-                            <h3 class="col card-price">$${pokemonData.price}</h3>
-                            <h3 class="col card-quantity" id="card-quantity-${pokemonData.id}">Qty: ${pokemon.quantity}</h3>
-                            <h3 class="col card-total-price"> Total: $${(pokemonData.price * pokemon.quantity).toFixed(2)}</h3>
-                        </div>
-                    </div>
-                    `;
-                    $(`#order-${index + 1}`).append(entry);
-                })*/
-            });
+        $("#current-points").text(data.points);
+        $("#desired-points").text(data.desired_reward_cost);
+        move(Math.min(Math.floor((data.points / data.desired_reward_cost) * 100), 100));
+        data.rewards_pending.forEach(async (reward, index) => {
+            let date = new Date(reward.redeem_date)
+            let dateTime = date.toString().split(" ")
+            let element = `
+                <div class="order" id="order-${index + 1}" style="text-align: center">
+                    <h3>${reward.reward_name}</h3>
+                    <h2>${dateTime[1] + " " + dateTime[2] + ", " + dateTime[3]}</h2>
+                    <p class="details">${reward.reward_desc}</p>
+                </div>`;
+            $("#pending-rewards").append(element);
         });
+    });
 }
 
 async function loadRewards() {
@@ -81,18 +59,18 @@ async function loadRewards() {
 }
 
 async function loadDashboard() {
-    // await loadPoints();
+    await loadPoints();
     await loadRewards();
 }
 
 loadDashboard();
 
-function move() {
+function move(percentage) {
     var elem = document.getElementById("myBar");   
     var width = 20;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= 90) {
+      if (width >= percentage) {
         clearInterval(id);
       } else {
         width++; 
@@ -101,6 +79,3 @@ function move() {
       }
     }
   }
-  window.onload = function() {
-    move();
-  };
